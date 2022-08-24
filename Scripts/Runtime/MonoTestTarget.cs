6ed785cca2991544c2f1c9eaaf312da43a02665b
revertan3 <no.esis323@gmail.com>
2022-08-24 22:26:57 +0900

    実際に動くかのテスト

--- Scripts/Runtime/MonoTestTarget.cs
+++ Scripts/Runtime/MonoTestTarget.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace AllMonoChanger.Scripts.Runtime
@@ -5,5 +6,17 @@ namespace AllMonoChanger.Scripts.Runtime
     public class MonoTestTarget : MonoBehaviour
     {
         [SerializeField] private int _testValue;
+    }
+
+    [ChangerTarget(typeof(MonoTestTarget))]
+    public class TestChanger : BaseAllChanger
+    {
+        public override void Change(SerializedObject serializedObject)
+        {
+            serializedObject.Update();
+            var testValueProp = serializedObject.FindProperty("_testValue");
+            testValueProp.intValue *= 2;
+            serializedObject.ApplyModifiedProperties();
+        }
     }
 }
