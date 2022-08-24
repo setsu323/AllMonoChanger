fdf2e82c1c063e02fc1c17882e9981e4d61756d6
revertan3 <no.esis323@gmail.com>
2022-08-24 19:17:52 +0900

    全てのComponentとScriptableObjectを取得するためのヘルパークラスを作成

--- Scripts/Editor/ChangeHelper.cs
+++ Scripts/Editor/ChangeHelper.cs
@@ -1,7 +1,36 @@
+using System;
+using System.Collections.Generic;
+using PlasticGui;
+using UnityEditor;
+using UnityEngine;
+
 namespace AllMonoChanger.Scripts.Editor
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ChangeHelper
     {
-        
+        public static IEnumerable<Component> GetAllComponent(Type componentType)
+        {
+            var guids = AssetDatabase.FindAssets("t:Prefab");
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var gameObject = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                yield return gameObject.GetComponentInChildren(componentType, true);
+            }
+        }
+
+        public static IEnumerable<ScriptableObject> GetAllScriptableObject(Type scriptableObjectType)
+        {
+            var name = scriptableObjectType.Name;
+            var guids = AssetDatabase.FindAssets("t:" + name);
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                yield return AssetDatabase.LoadAssetAtPath(path, scriptableObjectType) as ScriptableObject;
+            }
+        }
     }
 }
