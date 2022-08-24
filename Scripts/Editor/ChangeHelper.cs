7fe3f6d82afd062b0d87351d1e42aeddd61bbb52
revertan3 <no.esis323@gmail.com>
2022-08-24 18:22:00 +0900

    ひな形作成

---
+++ Scripts/Editor/ChangeHelper.cs
@@ -0,0 +1,7 @@
+namespace AllMonoChanger.Scripts.Editor
+{
+    public class ChangeHelper
+    {
+        
+    }
+}
---
+++ Scripts/Editor/ChangeWindow.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace AllMonoChanger.Scripts.Editor
+{
+    /// <summary>
+    /// 全てのPrefabの中から、特定の手順で値を上書きするための機能。
+    /// </summary>
+    public class ChangeWindow : EditorWindow
+    {
+        public void GetChangeFiled(){}
+    }
+}
